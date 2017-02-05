f8f27c31deeeaaf266d9c3c5d0807d2f5ff510c9
AJ <anders89jensen@gmail.com>
2017-02-05 14:55:40 +0100

    now image is working

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src="AAA.jpg" className="App-logo" alt="me" />
-          <h2>Welcome to React</h2>
+          <img src={require("./AAA.jpg")} className="App-logo" alt="me" />
+          <h2>Anders Jensen</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          my text
         </p>
       </div>
     );
